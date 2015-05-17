7000874c44a551801f3aaa981ef77d053f08df2e
TheCreeper <dc0@firebit.co.uk>
2015-05-17 01:46:13 +0100

    Added documentation for IsOk and IsErr.

--- util.go
+++ util.go
@@ -2,6 +2,7 @@ package pop3
 
 import "strings"
 
+// IsOk checks to see if the reply from the server contains +OK.
 func IsOK(s string) bool {
 
 	if strings.Fields(s)[0] != OK {
@@ -12,6 +13,7 @@ func IsOK(s string) bool {
 	return true
 }
 
+// IsErr checks to see if the reply from the server contains +Err.
 func IsErr(s string) bool {
 
 	if strings.Fields(s)[0] != ERR {
